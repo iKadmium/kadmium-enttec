299062b106f76999f7037e09fb65adbf7715725d
Jesse Higginson <kadmium@gmail.com>
2022-04-28 13:03:13 +1000

    Fix output

---
+++ Kadmium-Enttec.DemoApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Kadmium_Enttec;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+IEnttecWriter writer = new EnttecWriter();
+await writer.OpenAsync("COM3");
+byte[] payload = new byte[500];
+payload[0] = 255;
+payload[1] = 255;
+
+while (true)
+{
+	await writer.WriteAsync(payload);
+	await Task.Delay(1000 / 40);
+}
